401132d2092ffc99856f5b8690b464e141a4ee30
song <162600706+song6-1@users.noreply.github.com>
2025-03-17 18:16:00 +0900

    Update priority_queue.py

--- priority_queue.py
+++ priority_queue.py
@@ -22,7 +22,7 @@ class PriorityQueue:
   def _sift_up(self, index):
     while index > 0:
       parent_index = (index - 1) // 2
-      if self.heap[parent_index][0] < self.heap[index][0]:
+      if self.heap[parent_index][0] > self.heap[index][0]:
         self._swap(parent_index, index)
         index = parent_index
       else:
