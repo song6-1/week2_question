94fc93d49127df5d8a13f92e6afb763c6a6b6148
song <162600706+song6-1@users.noreply.github.com>
2025-03-17 17:51:23 +0900

    Update priority_queue.py

--- priority_queue.py
+++ priority_queue.py
@@ -11,7 +11,6 @@ class PriorityQueue:
     if len(self.heap) > 1:
       self._swap(0, len(self.heap) - 1)
       item = self.heap.pop()
-      print(type(item))
       self._sift_down(0)
       return item
     elif len(self.heap) == 1:
